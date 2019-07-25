fd7e9d92f1bcff2b1b9da52d0afc419aebf34690
mikeutj8 <mike.utj@gmail.com>
2019-07-25 08:47:19 -0500

    Mike

--- src/Modelo/AgregarCitaDAO.java
+++ src/Modelo/AgregarCitaDAO.java
@@ -1,10 +1,16 @@
 
 package Modelo;
 
+import Vista.AgregarCita;
+
 /**
  *
  * @author CST-UTJ
  */
 public class AgregarCitaDAO {
+    private AgregarCita jf;
     
+    public AgregarCitaDAO(AgregarCita jf){
+        this.jf=jf;
+    }
 }
