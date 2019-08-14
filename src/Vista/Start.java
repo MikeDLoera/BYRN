be99108498255da45c70c35f9fe71bdfc9afc922
Samlmld <alansamaf.utj@gmail.com>
2019-08-14 18:48:50 -0500

    Detalles

--- src/Vista/Start.java
+++ src/Vista/Start.java
@@ -1,6 +1,9 @@
 
 package Vista;
 
+import java.awt.Image;
+import java.awt.Toolkit;
+
 /**
  *
  * @author CST-UTJ
@@ -26,6 +29,7 @@ public class Start extends javax.swing.JFrame {
 
         contenedor = new javax.swing.JPanel();
 
+        setIconImage(getIconImage());
         setUndecorated(true);
 
         contenedor.setBackground(new java.awt.Color(255, 255, 255));
@@ -50,11 +54,14 @@ public class Start extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+        
+        
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
+        
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -72,16 +79,26 @@ public class Start extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(Start.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+        
+        
+   
+                
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
+                
                 new Start().setVisible(true);
             }
-        });
+            
+                
+        }
+        );
     }
+     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public javax.swing.JPanel contenedor;
     // End of variables declaration//GEN-END:variables
 }
+
+
